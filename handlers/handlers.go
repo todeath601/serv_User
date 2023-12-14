999895f8bbb797fbc13683ea03d052ad1e51fc6c
todeath <helpmedierightnow601@gmail.com>
2023-12-14 15:22:32 +0300

    2
    storage

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,10 +1,18 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Storage interface {
+	Create() user
+	Read() user
+	ReadOne() (user, error)
+	Delete() user
+}
 
 type user struct {
 	ID        string `json:"id"`
@@ -13,8 +21,51 @@ type user struct {
 	Age       int    `json:"age"`
 }
 
-type error struct {
-	Error string `json:"error`
+type MemoryStorage struct {
+	users []user
+}
+
+func (s MemoryStorage) Create(us user) user {
+	s.users = append(s.users, us)
+	return us
+
+}
+
+func (s MemoryStorage) ReadOne(id string) (user, error) {
+	for _, a := range s.users {
+		if a.ID == id {
+			return a, nil
+		}
+	}
+	return user{}, errors.New("not found")
+}
+
+func (s MemoryStorage) Delete(id string) error {
+	for i, a := range s.users {
+		if a.ID == id {
+			s.users = append(s.users[:i], s.users[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("not found")
+
+}
+
+func (s MemoryStorage) Read() []user {
+	return s.users
+}
+
+func NewMemoryStorage() MemoryStorage {
+	var users = []user{
+		{ID: "1", FirstName: "Vladimir", LastName: "Sakhonchyk", Age: 24},
+		{ID: "2", FirstName: "Nikita", LastName: "Samokhvalov", Age: 25},
+		{ID: "3", FirstName: "Alina", LastName: "Makarenko", Age: 22},
+	}
+	return MemoryStorage{users: users}
+}
+
+type HttpError struct {
+	Error string `json:"error"`
 }
 
 var users = []user{
@@ -24,39 +75,50 @@ var users = []user{
 }
 
 func GetUsers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, users)
+	storage := NewMemoryStorage()
+	c.IndentedJSON(http.StatusOK, storage.Read())
 }
 
 func DeleteUsersById(c *gin.Context) {
 	id := c.Param("id")
-	for i, a := range users {
-		if a.ID == id {
-			users = append(users[:i], users[i+1:]...)
-			c.IndentedJSON(http.StatusNoContent, a)
-			return
-		}
+	storage := NewMemoryStorage()
+	err := storage.Delete(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, HttpError{"not found"})
+
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, error{"not found"})
+	c.IndentedJSON(http.StatusNoContent, user{})
 }
 
 func GetUsersById(c *gin.Context) {
 	id := c.Param("id")
+	storage := NewMemoryStorage()
+	user, err := storage.ReadOne(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, HttpError{"not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, user)
+
 	for _, a := range users {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
+			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, error{"not found"})
+	c.IndentedJSON(http.StatusNotFound, HttpError{"not found"})
 }
 
 func PostUsers(c *gin.Context) {
 	var newUser user
 	err := c.BindJSON(&newUser)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, error{"bad request"})
+		c.IndentedJSON(http.StatusBadRequest, HttpError{"bad request"})
 		return
 	}
-	users = append(users, newUser)
+	storage := NewMemoryStorage()
+	storage.Create(newUser)
 	c.IndentedJSON(http.StatusCreated, newUser)
 }
 
--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-
+func getRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/users", handlers.GetUsers)
 	router.GET("/users/:id", handlers.GetUsersById)
 	router.POST("/users", handlers.PostUsers)
 	router.DELETE("/users/:id", handlers.DeleteUsersById)
+	return router
+}
+
+func main() {
+	router := getRouter()
 	router.Run(":8080")
 }
